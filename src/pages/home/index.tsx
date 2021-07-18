88a02670696224cb49c81ade50e565f149fdc2d8
Matt Granmoe <mattgranmoe@gmail.com>
2021-07-18 16:17:07 -0500

    Update <title>

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Next.js Starter</title>
+        <title>Stripe Code Exercise</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
